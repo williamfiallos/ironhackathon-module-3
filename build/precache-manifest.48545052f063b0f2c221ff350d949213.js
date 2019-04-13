b4e7c124a32e6d54c2791b6823a366ab63aa54ea
williamfiallos <wfiallos10@gmail.com>
2019-04-13 15:05:53 -0400

    new build files

---
+++ build/precache-manifest.48545052f063b0f2c221ff350d949213.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/williamfiallos/ironhackathon-module-3/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "e2c4ce31c4bb730e2cb7",
+    "url": "/williamfiallos/ironhackathon-module-3/static/js/runtime~main.3d220ae6.js"
+  },
+  {
+    "revision": "e788c823cce88f889287",
+    "url": "/williamfiallos/ironhackathon-module-3/static/js/main.b1008a25.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/williamfiallos/ironhackathon-module-3/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "e788c823cce88f889287",
+    "url": "/williamfiallos/ironhackathon-module-3/static/css/main.d0fe0dcb.chunk.css"
+  },
+  {
+    "revision": "7b0ecb3a7d13ee84e9f53bd0c3b50f7b",
+    "url": "/williamfiallos/ironhackathon-module-3/index.html"
+  }
+];
